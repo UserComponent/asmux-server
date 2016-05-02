ed0128df2f2f769fc5de9bcdb21c461a8b635b00
Anthony Hendrickson <ahendrickson@ausinc.com>
2016-05-02 01:47:56 -0400

    Stable open/read/write index.html file

--- server.asm
+++ server.asm
@@ -3,19 +3,22 @@
 ; Assemble with `nasm -f elf64 server.asm`
 ; Link with `ld -o asmux-server server.o`
 ; @author Anthony Hendrickson <ahendrickson@ausinc.com>
-; @version 0.1.0
+; @version 0.2.0
 ;=======================================================
 
         CPU     x64
 
         SECTION .data
 
+        ; File / Socket Variables
         filename        db      "index.html", 0
         filepntr        dq      0
         sock            dq      0
         client          dq      0
         sockopt_off     dw      0
         sockopt_on      dw      1
+
+        ; Server / System Constants
         MAX_CLIENTS     equ     10
         BUFLEN          equ     512
         __O_RDONLY      equ     00
@@ -35,7 +38,7 @@
         __NR_close      equ     3
         __NR_socket     equ     41
         __NR_accept     equ     43
-        __NR_shutdown   equ     48      ; @TODO replace close() with shutdown()
+        __NR_shutdown   equ     48      ; @TODO replace close() with shutdown() on sockets
         __NR_bind       equ     49
         __NR_listen     equ     50
         __NR_setsockopt equ     54
@@ -144,7 +147,6 @@ _server_accept:
         mov     rdx, [reqlen]
         syscall
 
-
         ; @TODO handle headers from request buffer
 
         ; Open index.html into buffer
@@ -156,13 +158,14 @@ _server_accept:
         jle     _client_close           ; @TODO This should be HTTP 404
         mov     [filepntr], rax
 
+        mov     rcx, qword 0
+
 _read_html:
 
         SECTION .data
 
-        resbuf  TIMES   buflen  \
+        resbuf  TIMES   BUFLEN  \
                 db      0
-        reslen  dw      0
 
 
         SECTION .text
@@ -171,24 +174,22 @@ _read_html:
         mov     rax, __NR_read
         mov     rdi, [filepntr]
         mov     rsi, resbuf
-        mov     rdx, buflen
+        mov     rdx, BUFLEN
         syscall
 
-        cmp     rax, 0                  ; Check for error
-        jl      _client_close           ; Read-error
+        cmp     rax, 1                  ; Check for error / end of reading/writing
+        jl      _client_close           ; Continue to close() if done
 
-        mov     [reslen], rax           ; Update response buffer length
-
-        cmp     rax, 0                  ; Continue if done reading
-        jg      _read_html              ; Else, keep on reading!
-
+        mov     rcx, rax                ; Update response buffer length
 
         ; Write output buffer to client
         mov     rax, __NR_write
         mov     rdi, [client]
         mov     rsi, resbuf
-        mov     rdx, [reslen]
+        mov     rdx, rcx
         syscall
+
+        jmp      _read_html              ; Otherwise, keep on reading / writing!
 
 _client_close:
 
